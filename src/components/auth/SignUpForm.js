9bfc164fabccf4e028da4195c950aec3703624a1
Edgar Mueller <edgar.m.mueller@gmail.com>
2018-12-22 18:42:12 +0100

    Improve sign up page

--- src/components/auth/SignUpForm.js
+++ src/components/auth/SignUpForm.js
@@ -1,115 +1,116 @@
-import React from 'react';
-import Radium from 'radium';
-import { Link } from 'react-router-dom';
-import Button from '@material-ui/core/Button';
-import TextField from '@material-ui/core/TextField';
-import withStyles from '@material-ui/core/styles/withStyles';
-import PropTypes from 'prop-types';
+import React from "react";
+import _ from "lodash";
+import Radium from "radium";
+import { Link } from "react-router-dom";
+import Button from "@material-ui/core/Button";
+import FormControl from "@material-ui/core/FormControl";
+import FormHelperText from "@material-ui/core/FormHelperText";
+import Input from "@material-ui/core/Input";
+import InputLabel from "@material-ui/core/InputLabel";
+import withStyles from "@material-ui/core/styles/withStyles";
+import red from "@material-ui/core/colors/red";
+import PropTypes from "prop-types";
 
 const RadiumLink = Radium(Link);
 
 const styles = {
   button: {
-    textDecoration: 'none',
-    color: '#333435',
-    paddingLeft: '1em',
-    paddingRight: '1em',
-    display: 'inline-flex',
-    marginLeft: '1em',
-    marginRight: '1em',
-    height: '36px',
-    fontWeight: 'bold',
-    '&:hover': {
-      backgroundColor: '#80CBC4',
-      color: '#fff',
+    textDecoration: "none",
+    color: "#333435",
+    paddingLeft: "1em",
+    paddingRight: "1em",
+    display: "inline-flex",
+    marginLeft: "1em",
+    marginRight: "1em",
+    height: "36px",
+    fontWeight: "bold",
+    "&:hover": {
+      backgroundColor: "#80CBC4",
+      color: "#fff"
     },
-    alignItems: 'center',
-    textTransform: 'uppercase',
-    fontSize: '0.8em',
-    letterSpacing: '0.065em',
+    alignItems: "center",
+    textTransform: "uppercase",
+    fontSize: "0.8em",
+    letterSpacing: "0.065em"
   },
+  form: {
+    display: "flex",
+    flexDirection: "column",
+    maxWidth: 280,
+    margin: "0 auto"
+  }
 };
 
 export class SignUpForm extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      name: undefined,
-      email: undefined,
-      password: undefined,
-      confirmationPassword: undefined
-    };
-  }
-
-  handleUpdateName = event => {
-    this.setState({
-      name: event.target.value
-    })
-  };
-
-  handleUpdateEmail = event => {
-    this.setState({
-      email: event.target.value,
-    });
-  };
-
-  handleUpdatePassword = (event) => {
-    this.setState({
-      password: event.target.value,
-    })
-  };
-
-  handleUpdateConfirmationPassword = event => {
-    this.setState({
-      confirmationPassword: event.target.value,
-    });
-  };
-
   render() {
-    const { classes, handleFormSubmit } = this.props;
+    const {
+      classes,
+      handleSubmit,
+      handleUpdateName,
+      handleUpdateEmail,
+      handleUpdatePassword,
+      handleUpdateConfirmationPassword,
+      errors,
+      name,
+      mail
+    } = this.props;
 
     return (
       <div>
-        <TextField
-          required
-          type="text"
-          label="Name"
-          onChange={this.handleUpdateName}
-        />
-        <br />
-        <TextField
-          required
-          type="text"
-          label="Mail Address"
-          onChange={this.handleUpdateEmail}
-        />
-        <br />
-        <TextField
-          required
-          type="password"
-          label="Password"
-          onChange={this.handleUpdatePassword}
-        />
-        <br />
-        <TextField
-          required
-          type="password"
-          label="Confirm Password"
-          onChange={this.handleUpdateConfirmationPassword}
-        />
-        <br />
+        <div className={classes.form}>
+          <FormControl required error={!_.isEmpty(errors.name)}>
+            <InputLabel htmlFor="name">Name</InputLabel>
+            <Input
+              id="name"
+              onChange={handleUpdateName}
+              placeholder="Enter your name"
+              defaultValue={name}
+            />
+            <FormHelperText id="error-name">{errors.name}</FormHelperText>
+          </FormControl>
+          <FormControl required error={!_.isEmpty(errors.mail)}>
+            <InputLabel htmlFor="mail">Mail</InputLabel>
+            <Input
+              id="mail"
+              onChange={handleUpdateEmail}
+              placeholder="Enter your mail address"
+              defaultValue={mail}
+            />
+            <FormHelperText id="error-mail">{errors.mail}</FormHelperText>
+          </FormControl>
+          <FormControl required error={!_.isEmpty(errors.password)}>
+            <InputLabel htmlFor="password">Password</InputLabel>
+            <Input id="password" onChange={handleUpdatePassword} />
+            <FormHelperText id="error-passowrd">
+              {errors.password}
+            </FormHelperText>
+          </FormControl>
+          <FormControl required error={!_.isEmpty(errors.confirmationPassword)}>
+            <InputLabel htmlFor="confirm-password">Confirm Password</InputLabel>
+            <Input
+              id="confirm-password"
+              onChange={handleUpdateConfirmationPassword}
+            />
+            <FormHelperText id="error-confirm-passowrd">
+              {errors.confirmationPassword}
+            </FormHelperText>
+          </FormControl>
+        </div>
 
-        <div style={{ marginTop: '1em' }}>
-            <RadiumLink to="/auth/recover/password" className={classes.button}>
-              I already have an account
-            </RadiumLink>
-            <Button
-              type="submit"
-              className={classes.button}
-              onClick={() => handleFormSubmit(this.state.name, this.state.email, this.state.password)}
-            >
-              Sign Up
-            </Button>
+        <div style={{ marginTop: "1em" }}>
+          <RadiumLink
+            to="/auth/recover/password"
+            className={classes.button}
+            style={{ borderRadius: 4 }}
+          >
+            I already have an account
+          </RadiumLink>
+          <Button className={classes.button} onClick={handleSubmit}>
+            Sign Up
+          </Button>
+        </div>
+        <div style={{ color: red[500] }}>
+          {errors.message}
         </div>
       </div>
     );
@@ -117,8 +118,32 @@ export class SignUpForm extends React.Component {
 }
 
 SignUpForm.propTypes = {
-  handleFormSubmit: PropTypes.func.isRequired,
+  handleSubmit: PropTypes.func.isRequired,
+  handleUpdateName: PropTypes.func.isRequired,
+  handleUpdateEmail: PropTypes.func.isRequired,
+  handleUpdatePassword: PropTypes.func.isRequired,
+  handleUpdateConfirmationPassword: PropTypes.func.isRequired,
   classes: PropTypes.shape({}).isRequired,
+  errors: PropTypes.shape({
+    message: PropTypes.string,
+    name: PropTypes.string,
+    mail: PropTypes.string,
+    password: PropTypes.string,
+    confirmationPassword: PropTypes.string
+  }),
+  name: PropTypes.string,
+  mail: PropTypes.string
+};
+
+SignUpForm.defaultProps = {
+  errors: {
+    message: null,
+    mail: null,
+    password: null,
+    name: null
+  },
+  name: "",
+  mail: ""
 };
 
 export default withStyles(styles)(SignUpForm);
--- src/components/auth/SignUpPage.js
+++ src/components/auth/SignUpPage.js
@@ -1,77 +1,156 @@
-import React from 'react';
-import { connect } from 'react-redux';
-import { routerActions } from 'react-router-redux';
+import React from "react";
+import _ from "lodash";
+import { connect } from "react-redux";
+import { routerActions } from "react-router-redux";
+import mailValidator from "email-validator";
 
 import SignUpForm from "./SignUpForm";
-import {signUpWithEmail} from "../../api";
+import { signUpWithEmail } from "../../api";
+
+const getError = (errorData, fieldName) => {
+  if (errorData && _.has(errorData.details, `obj.${fieldName}`)) {
+    const error = errorData.details[`obj.${fieldName}`][0].msg[0];
+    if (error === "error.path.missing") {
+      return "Field missing";
+    }
+    return error;
+  }
+  return null;
+};
 
 class SignUpPage extends React.Component {
-
   state = {
+    name: undefined,
+    mail: undefined,
+    password: undefined,
+    confirmationPassword: undefined,
     isLoading: false,
     success: undefined,
-    error: undefined
+    errorMsg: undefined,
+    errors: {}
   };
 
-  handleFormSubmit = (name, mail, password) => {
+  handleUpdateName = event => {
+    this.setState({
+      name: event.target.value,
+      errors: {
+        ...this.state.errors,
+        name: null
+      }
+    });
+  };
+
+  handleUpdateEmail = event => {
+    const value = event.target.value;
+    const isValidMail = mailValidator.validate(value);
+    this.setState({
+      mail: value,
+      errors: {
+        ...this.state.errors,
+        mail: isValidMail ? null : "Invalid mail address"
+      }
+    });
+  };
+
+  handleUpdatePassword = event => {
+    this.setState({
+      password: event.target.value,
+      errors: {
+        ...this.state.errors,
+        password: null
+      }
+    });
+  };
+
+  handleUpdateConfirmationPassword = event => {
+    this.setState({
+      confirmationPassword: event.target.value
+    });
+  };
+
+  handleSubmit = () => {
     this.setState({ isLoading: true });
-    signUpWithEmail(name, mail, password)
-      .then(
-        () => this.setState({
+    signUpWithEmail(this.state.name, this.state.mail, this.state.password).then(
+      () =>
+        this.setState({
           isLoading: false,
           success: true,
-          error: undefined
+          errorMsg: null,
+          errors: null
         }),
-        error => {
-          this.setState({
-            isLoading: false,
-            success: false,
-            error: error.response.data.description || error.response.statusText
-          })
-        }
-      )
+      error => {
+        const errorData = _.get(error, 'response.data');
+        const mail = getError(errorData, "email");
+        const name = getError(errorData, "name");
+        const password = getError(errorData, "password");
+        this.setState({
+          isLoading: false,
+          success: false,
+          errorMsg: error.message,
+          password: "",
+          confirmationPassword: "",
+          errors: {
+            name,
+            mail,
+            password
+          }
+        });
+      }
+    );
   };
 
   render() {
     if (this.state.isLoading) {
-      return (<div>Loading...</div>)
+      return <div>Loading...</div>;
     }
 
     if (!this.state.isLoading && this.state.success) {
-      return <div>Please check your email</div>
+      return <div>Please check your email</div>;
     }
 
     if (!this.state.isLoading && this.state.error === false) {
-      return <div>An error occurred while signing you up</div>
+      return <div>An error occurred while signing you up</div>;
     }
 
     return (
       <div>
-        <h2 style={{ paddingTop: '1em' }}>Welcome to egometer</h2>
+        <h2>Sign up</h2>
         <SignUpForm
-          handleFormSubmit={this.handleFormSubmit}
+          name={this.state.name}
+          mail={this.state.mail}
+          handleSubmit={this.handleSubmit}
+          handleUpdateName={this.handleUpdateName}
+          handleUpdateEmail={this.handleUpdateEmail}
+          handleUpdatePassword={this.handleUpdatePassword}
+          handleUpdateConfirmationPassword={
+            this.handleUpdateConfirmationPassword
+          }
+          errors={{
+            ...this.state.errors,
+            message: this.state.errorMsg,
+            confirmationPassword:
+              this.state.password !== this.state.confirmationPassword
+                ? "Passwords do not match"
+                : null
+          }}
         />
-        {
-          this.state.error && (<p style={{ paddingTop: '1em', color: 'red' }}>{this.state.error}</p>)
-        }
+        {this.state.error && (
+          <p style={{ paddingTop: "1em", color: "red" }}>{this.state.error}</p>
+        )}
       </div>
     );
   }
 }
 
-SignUpPage.propTypes = {
+SignUpPage.propTypes = {};
 
-};
-
-SignUpPage.defaultProps = {
-
-};
+SignUpPage.defaultProps = {};
 
 const mapDispatchToProps = {
-  replace: routerActions.replace,
+  replace: routerActions.replace
 };
 
 export default connect(
   null,
-  mapDispatchToProps,
+  mapDispatchToProps
 )(SignUpPage);
