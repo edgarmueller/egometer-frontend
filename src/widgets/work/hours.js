76c3cf28ff3ab1a9a8f6b8bdaa5630c1679cdb82
Edgar Mueller <edgar.m.mueller@gmail.com>
2018-12-23 16:47:49 +0100

    Add number slide widget, configure hours widget

--- src/widgets/work/hours.js
+++ src/widgets/work/hours.js
@@ -2,10 +2,12 @@ import React from "react";
 import TimerIcon from "@material-ui/icons/Timer";
 import NumberCell from "../../components/widgets/cells/NumberCell";
 import { isNumberOrInteger } from "../../common/testers";
-import numberWidget from "../common/numberWidget";
+import numberSliderWidget from "../common/numberSliderWidget";
+import NumberSlider, { withMinMaxStep } from "../../components/widgets/daily/NumberSlider";
 
 const WorkingHoursWidget = {
-  ...numberWidget,
+  ...numberSliderWidget,
+  day: withMinMaxStep({ min: 0, max: 16, step: 0.25 })(NumberSlider),
   name: "WorkingHours",
   schemaId: "MinimumOf0",
   label: "Working Hours",
