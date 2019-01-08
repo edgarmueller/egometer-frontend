f861ad1bb96037048ea587d8b2670cd884600ee4
Edgar Mueller <edgar.m.mueller@gmail.com>
2019-01-08 20:05:28 +0100

    Update day property of water consumption widget

--- src/widgets/health/water.js
+++ src/widgets/health/water.js
@@ -1,9 +1,13 @@
 import React from "react";
 import Icon from "@material-ui/icons/LocalDrink";
-import numberWidget from "../common/numberWidget";
+import numberSliderWidget from "../common/numberSliderWidget";
+import NumberSlider, {
+  withMinMaxStep
+} from "../../components/widgets/daily/NumberSlider";
 
 const WaterConsumptionWidget = {
-  ...numberWidget,
+  ...numberSliderWidget,
+  day: withMinMaxStep({ min: 0, max: 4, step: 0.25 })(NumberSlider),
   name: "WaterConsumption",
   label: "Water Consumption",
   icon: <Icon />, // use icon in renderer as well
