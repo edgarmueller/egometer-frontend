bec5e7d6d57de77fffe150a468419c459385edb6
Edgar Mueller <edgar.m.mueller@gmail.com>
2019-01-08 20:04:47 +0100

    Clean up number slider widget

--- src/widgets/common/numberSliderWidget.js
+++ src/widgets/common/numberSliderWidget.js
@@ -1,14 +1,11 @@
 import { isNumber } from "../../common/testers";
 import NumberBarChart from "../../components/visualizations/NumberBarChart";
 import NumberCell from "../../components/widgets/cells/NumberCell";
-import NumberSlider, { withMinMaxStep } from "../../components/widgets/daily/NumberSlider";
 
-const NumberWidget = {
+export default {
   schemaId: "Number",
   month: NumberBarChart,
   cell: NumberCell,
   isApplicable: s => (isNumber(s) ? 1 : -1),
   h: 1
 };
-
-export default NumberWidget;
