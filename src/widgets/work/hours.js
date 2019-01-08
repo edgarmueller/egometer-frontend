5844a5b52e4488ab3c2fee0d277d41260790aa2b
Edgar Mueller <edgar.m.mueller@gmail.com>
2019-01-08 20:12:00 +0100

    Use number widget instead of number slider widget for hours

--- src/widgets/work/hours.js
+++ src/widgets/work/hours.js
@@ -2,12 +2,10 @@ import React from "react";
 import TimerIcon from "@material-ui/icons/Timer";
 import NumberCell from "../../components/widgets/cells/NumberCell";
 import { isNumberOrInteger } from "../../common/testers";
-import numberSliderWidget from "../common/numberSliderWidget";
-import NumberSlider, { withMinMaxStep } from "../../components/widgets/daily/NumberSlider";
+import numberWidget from "../common/numberWidget";
 
 const WorkingHoursWidget = {
-  ...numberSliderWidget,
-  day: withMinMaxStep({ min: 0, max: 16, step: 0.25 })(NumberSlider),
+  ...numberWidget,
   name: "WorkingHours",
   schemaId: "MinimumOf0",
   label: "Working Hours",
