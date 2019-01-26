6de3d664498d3b210829dd92414311a0312bd6b1
Edgar Mueller <edgar.m.mueller@gmail.com>
2019-01-26 19:48:35 +0100

    Update DefaultErrorBoundary page

--- src/components/common/DefaultErrorBoundary.js
+++ src/components/common/DefaultErrorBoundary.js
@@ -1,53 +1,62 @@
-import React from 'react';
-import Typography from '@material-ui/core/Typography';
-import withStyles from '@material-ui/core/styles/withStyles';
-import red from '@material-ui/core/colors/red';
-import ErrorIcon from '@material-ui/icons/Error';
+import React from "react";
+import Typography from "@material-ui/core/Typography";
+import withStyles from "@material-ui/core/styles/withStyles";
+import red from "@material-ui/core/colors/red";
+import ErrorIcon from "@material-ui/icons/Error";
 import Ionicon from "react-ionicons";
 import Radium from "radium";
 import { Link } from "react-router-dom";
 
-import { logo } from '../../common/styles';
+import { logo } from "../../common/styles";
 
 export const RadiumLink = Radium(Link);
 
 const styles = {
-    logo
-}
+  logo
+};
 
 class DefaultErrorBoundary extends React.Component {
-    state = {
-        isError: false
-    }
+  state = {
+    isError: false
+  };
 
-    static getDerivedStateFromError() {
-        return { isError : true };
-    }
+  static getDerivedStateFromError(error) {
+    console.log(error);
+    return { isError: true };
+  }
 
-    render() {
-        const { isError } = this.state;
-        const { children, classes } = this.props;
-        return isError ? 
-            <div style={{ display: 'flex', alignItems: 'center', flexDirection: 'column', paddingTop: '2em' }}>
-                <RadiumLink className={classes.logo} to="/">
-                  <div
-                    style={{
-                      display: "flex",
-                      flexDirection: "column",
-                      alignItems: "center"
-                    }}
-                  >
-                    <Ionicon icon="md-flash" />
-                    egometer
-                  </div>
-                </RadiumLink>
-                <h1 style={{ color: red[500]}}>
-                    We are sorry, but something went wrong :(
-                </h1>
-                <ErrorIcon style={{ color: red[500], fontSize: 50 }} />
-                <Typography variant='body1'>Please try again later</Typography>
-            </div> : children
-    }
+  render() {
+    const { isError } = this.state;
+    const { children, classes } = this.props;
+    return isError ? (
+      <div
+        style={{
+          display: "flex",
+          alignItems: "center",
+          flexDirection: "column",
+          paddingTop: "2em"
+        }}
+      >
+        <RadiumLink className={classes.logo} to="/">
+          <div
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center"
+            }}
+          >
+            <Ionicon icon="md-flash" />
+            egometer
+          </div>
+        </RadiumLink>
+        <h1 style={{ color: red[500] }}>Sorry, something went wrong :(</h1>
+        <ErrorIcon style={{ color: red[500], fontSize: 50 }} />
+        <Typography variant="body1">Please try again later</Typography>
+      </div>
+    ) : (
+      children
+    );
+  }
 }
 
-export default withStyles(styles)(DefaultErrorBoundary)
+export default withStyles(styles)(DefaultErrorBoundary);
