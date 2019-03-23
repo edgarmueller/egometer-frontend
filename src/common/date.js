58ad7d28a19e7973f65fb1c24dd742c4bfe1fd16
Edgar Mueller <edgar.m.mueller@gmail.com>
2019-03-23 14:17:54 +0100

    Correctly show displayed days in weekly dashboard

--- src/common/date.js
+++ src/common/date.js
@@ -11,7 +11,7 @@ export function daysInMonth(year, month) {
   return new Date(year, month, 0).getDate();
 }
 
-const findMonday = date => {
+export const findMonday = date => {
   if (date.getDay() === 1) {
     return date;
   }
