7d3314bf450fc4309d009caea89236eda871d873
Edgar Mueller <edgar.m.mueller@gmail.com>
2019-03-25 22:50:33 +0100

    Update NavBar styling

--- src/common/styles.js
+++ src/common/styles.js
@@ -1,20 +1,14 @@
 export const link = {
   textDecoration: "none",
-  color: "#000",
-  paddingLeft: "2em",
-  paddingRight: "2em",
-  display: "inline-flex",
+  color: "#fff",
   alignItems: "center",
-  marginLeft: "1em",
-  marginRight: "1em",
-  fontWeight: "bold",
-  fontSize: "0.875rem",
   "&:hover": {
-    backgroundColor: "#000",
+    backgroundColor: "rgba(255, 112, 67, 0.4)",
     color: "#fff"
   },
-  textTransform: "uppercase",
-  letterSpacing: "0.065em"
+  paddingLeft: "1rem",
+  paddingRight: "1rem",
+  fontSize: "0.8125rem"
 };
 
 export const buttonStyle = {
@@ -37,11 +31,10 @@ export const button = {
   height: "36px",
   fontWeight: "bold",
   "&:hover": {
-    backgroundColor: "#80CBC4",
+    backgroundColor: "rgba(85, 189, 253, 0.4)",
     color: "#fff"
   },
   alignItems: "center",
-  textTransform: "uppercase",
   fontSize: "0.8em",
   letterSpacing: "0.065em"
 };
@@ -74,13 +67,10 @@ export const display1 = {
 };
 
 export const logo = {
-  flex: 1,
   color: "#333435",
   borderRadius: "16px",
   paddingLeft: "2em",
   paddingRight: "2em",
-  paddingTop: "1em",
-  paddingBottom: "1em",
   marginRight: "1em",
   fontSize: "0.8125em",
   fontWeight: "bold",
