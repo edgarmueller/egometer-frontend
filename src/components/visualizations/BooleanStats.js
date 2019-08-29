348855561939051e7f259ad969f0b62be040d461
Edgar Mueller <edmue@Edgars-MBP.fritz.box>
2019-08-29 19:38:51 +0200

    Fix boolean streak calcuation

--- src/components/visualizations/BooleanStats.js
+++ src/components/visualizations/BooleanStats.js
@@ -1,5 +1,30 @@
 import React, { Fragment } from "react";
+import moment from "moment";
+import sortBy from "lodash/sortBy";
 import { lastDayOfMonth } from "../../common/date";
+
+const calcStreak = data => {
+  const sorted = sortBy(data, e => moment(e.date, "YYYY-MM-DD").toDate());
+  const streak = sorted.reduce(
+    ({ longest, current, prevDate }, { date, value }) => {
+      const day = moment(date, "YYYY-MM-DD").date();
+      if (day - 1 === prevDate || prevDate === -1) {
+        if (value) {
+          const curr = current + 1;
+          if (curr > longest) {
+            return { longest: curr, current: curr, prevDate: day };
+          }
+          return { longest, current: curr, prevDate: day };
+        } else {
+          return { longest, current: 0, prevDate: day };
+        }
+      }
+      return { longest, current: value ? 1 : 0, prevDate: day };
+    },
+    { longest: 0, current: 0, prevDate: -1 }
+  );
+  return streak.longest;
+};
 
 const BooleanStats = ({ data, days, meter, width }) => {
   if (!data) {
@@ -7,26 +32,14 @@ const BooleanStats = ({ data, days, meter, width }) => {
   }
   const monthDays = lastDayOfMonth(days[0].getMonth(), days[0].getFullYear());
   const yes = data.filter(({ value }) => value).length;
-  const streak = data.reduce(
-    ({ longest, current }, { value }) => {
-      if (value) {
-        const curr = current + 1;
-        if (curr > longest) {
-          return { current: curr, longest: curr };
-        }
-        return { longest, current: curr };
-      }
-      return { longest, current: 0 };
-    },
-    { longest: 0, current: 0 }
-  );
+  const streak = calcStreak(data);
   return (
     <Fragment>
       <div>
         <strong>Did so</strong>: {yes} out of {monthDays}
       </div>
       <div>
-        <strong>Longest streak</strong>: {streak.longest}
+        <strong>Longest streak</strong>: {streak}
       </div>
     </Fragment>
   );
