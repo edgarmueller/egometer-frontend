9d218af1bf71586a2dea993a9a8282e35a525391
Edgar Mueller <edgar.m.mueller@gmail.com>
2019-09-25 14:32:24 +0200

    Fix actions spec

--- src/__tests__/actions.spec.js
+++ src/__tests__/actions.spec.js
@@ -1,28 +1,27 @@
-import * as _ from 'lodash';
-import axios from 'axios';
-import configureStore from 'redux-mock-store';
-import thunk from 'redux-thunk';
-import MockAdapter from 'axios-mock-adapter';
+import * as _ from "lodash";
+import axios from "axios";
+import configureStore from "redux-mock-store";
+import thunk from "redux-thunk";
+import MockAdapter from "axios-mock-adapter";
 
-import * as actions from '../actions';
-import { BASE_URL } from '../constants';
-import { moodMeter, moodMeterId } from '../__mocks__/testData';
+import * as actions from "../actions";
+import { API_BASE_URL } from "../constants";
+import { moodMeter, moodMeterId } from "../__mocks__/testData";
 import mockStorage from "../__mocks__/mockStorage";
 
 global.localStorage = mockStorage();
 const middlewares = [thunk];
 const createMockStore = configureStore(middlewares);
 
-describe('delete meter', () => {
-  it('should cause DELETE request and update local state', () => {
+describe("delete meter", () => {
+  it("should cause DELETE request and update local state", async () => {
     const mock = new MockAdapter(axios);
-    mock.onDelete(`${BASE_URL}/meters/${moodMeterId}`)
-        .reply(200, moodMeter);
+    mock
+      .onDelete(`${API_BASE_URL}/meters/${moodMeterId}`)
+      .reply(200, moodMeter);
     const store = createMockStore({ meters: [moodMeter] });
-    store.dispatch(actions.deleteMeter(moodMeterId))
-         .then(() => {
-             const storedActions = store.getActions();
-             expect(_.head(storedActions).type).toBe(actions.DELETE_METER_SUCCESS);
-         })
+    await store.dispatch(actions.deleteMeter(moodMeterId));
+    const storedActions = store.getActions();
+    expect(_.head(storedActions).type).toBe(actions.DELETE_METER_SUCCESS);
   });
 });
