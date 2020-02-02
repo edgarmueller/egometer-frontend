bb5f38ce020198fdb0c78d723724f2641ba59890
Edgar Mueller <edgar.m.mueller@gmail.com>
2020-02-02 22:08:30 +0100

    Add jwt token expiration middleware

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,6 +1,7 @@
 import { compose, createStore, applyMiddleware } from 'redux';
 import logger from 'redux-logger';
 import thunk from 'redux-thunk';
+import jwtDecode from "jwt-decode";
 import createHistory from 'history/createBrowserHistory'
 import { routerMiddleware } from 'react-router-redux';
 
@@ -8,8 +9,25 @@ import app from '../reducers';
 import {createEpicMiddleware} from 'redux-observable';
 import {rootEpic} from "../epics";
 import * as api from '../api'
+import { USER_LOGGED_OUT } from '../actions';
 
 export const history = createHistory();
+
+const checkTokenExpirationMiddleware = store => next => action => {
+  const token = localStorage.getItem("egometer.token");
+  if (token) {
+    const decodedToken = jwtDecode(token);
+    console.log(decodedToken.exp)
+    if (decodedToken.exp < Date.now() / 1000) {
+      localStorage.removeItem("egometer.token");
+      localStorage.removeItem("egometer.role");
+      store.dispatch({
+        type: USER_LOGGED_OUT
+      });
+    }
+  }
+  next(action);
+};
 
 const epicMiddleware = createEpicMiddleware(rootEpic, { dependencies: { api }});
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
@@ -18,6 +36,7 @@ const configureStore = (initState) => {
   const middlewares = [
     thunk,
     epicMiddleware,
+    checkTokenExpirationMiddleware,
     routerMiddleware(history)
   ];
   if (process.env.NODE_ENV === 'development') {
